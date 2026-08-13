6f2a86e3d2407caec1b4817240028fb4ad1319bb
LaguertaDanielle <laguertadanielle08@gmail.com>
2026-08-13 20:44:27 +0800

    Add files via upload

---
+++ code_challege1.py
@@ -0,0 +1,3 @@
+baby = "\t\t\t\t\t\t\t\t\t\t\t\t\t*\n \t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n \t\t\t\t\t\t\t\t\t\t\t*           My name is:\t\t*\n \t\t\t\t\t\t\t\t\t\t*              \"Danielle laguerta\"\t\t*\n \t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t\t\t*\n \t\t\t\t\t\t\t\t\t\t*\t\t\t\t\t\t*\n \t\t\t\t\t\t\t\t\t\t\t*\t\t\t\t*\n \t\t\t\t\t\t\t\t\t\t\t\t*\t\t*\n\t\t\t\t\t\t\t\t\t\t\t\t\t*"
+
+print(baby)
