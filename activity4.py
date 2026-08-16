a7077fa548878a0e5b209c222cec6df50a6ef078
LaguertaDanielle <laguertadanielle08@gmail.com>
2026-08-16 16:07:28 +0800

    Add files via upload

---
+++ activity4.py
@@ -0,0 +1,11 @@
+# escape sequence
+#\n - new line
+#\t - tabs space
+#\r - carriage return 
+#\\ - insert slash
+#\b - back space
+#\ is different from /
+
+motto = "The quick brown \rfox jumps over the \b\b\b\blazy dog. \n\t\t\t\t\"A negative mind will never give you a positive life.\""
+
+print(motto)
