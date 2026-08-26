d288f49b00b0f512d4de7cac1652e445b99933bd
LaguertaDanielle <laguertadanielle08@gmail.com>
2026-08-26 18:04:59 +0800

    August 26, 2026

---
+++ activity5.py
@@ -0,0 +1,40 @@
+#Operators
+# + = Addition
+# - = Subtraction
+# * = Multiplication
+# / = Division
+# ** = Exponentation
+# // = Floor Division
+# % = Modulus
+
+a = 8
+b = 13
+
+sum = a + b
+
+print("The sum of ",a," and",b," is ",sum)
+
+sum = b - a
+
+print("The sum of ",b," and",a," is ", sum)
+
+sum = a * b
+
+print("The sum of ",a," and",b," is ", sum)
+
+sum = a/b
+
+print("The sum of ",a," and",b," is ", sum)
+
+sum = a**b
+
+print("The sum of ",a," and",b," is ", sum)
+
+sum = a//b
+
+print("The sum of ",a," and",b," is ", sum)
+
+sum = a%b
+
+print("The sum of ",a," and",b," is ", sum) 
+
